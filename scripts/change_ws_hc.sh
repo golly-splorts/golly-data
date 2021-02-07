03538ad6749d1e131183496683795d0ee7ae907e
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-02-07 01:06:30 -0800

    fix typo in script

--- scripts/change_ws_hc.sh
+++ scripts/change_ws_hc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$ find . \( -name "bracket*" -o -name "postseason*" \) -not -path "./vp/*" | xargs -n1 -I% gsed -i -e 's/World Series/Hellmouth Cup/g' %
+find . \( -name "bracket*" -o -name "postseason*" \) -not -path "./vp/*" | xargs -n1 -I% gsed -i -e 's/World Series/Hellmouth Cup/g' %
